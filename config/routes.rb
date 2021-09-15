799ac80f62b5f470745fedd4e5583501ddf81efa
samya cardoso de david <dedavidsamya@gmail.com>
2021-09-15 12:14:31 +0200

    created routes for home page, sunrise room page, sunset room page, and booking page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root to: "pages#home"
+
+  get '/sunset', to: "pages#sunset"
+  get '/sunrise', to: "pages#sunrise"
+  get '/booking', to: "pages#booking"
+
 end
+
+ # structure of routing:
+  # verb        to: "controller#action"
+  # get '/booking', to: "pages#booking"

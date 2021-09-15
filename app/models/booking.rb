f705acd648cafabd290b1f1295349dc6e88a6d34
samya cardoso de david <dedavidsamya@gmail.com>
2021-09-15 20:36:53 +0200

    created bookings controller

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,4 +2,7 @@ class Booking < ApplicationRecord
   validates :name, :email, :beginning, :end, :seats, :catering, presence: { message: "This field can't be empty"}
 end
 
-# Here I make sure that the user provides input in all fields.
+# This is my Booking model, which is directly connected to the structure of the Bookings table in my database
+## my Bookings table contains the columns name, email, beginning, end, seats, and catering, each one corresponding
+### to a field of the form which the user has access to, in order to book a meeting room.
+####  By adding the validation to all the columns I make sure that the user provides input in all fields of the form.

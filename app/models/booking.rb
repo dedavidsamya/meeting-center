56a216beddb49b3c8a19526e046c3f5cbe1ea92f
samya cardoso de david <dedavidsamya@gmail.com>
2021-09-15 20:29:58 +0200

    added validations to Booking model, so that no input field is left empty by the user

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,2 +1,5 @@
 class Booking < ApplicationRecord
+  validates :name, :email, :beginning, :end, :seats, :catering, presence: { message: "This field can't be empty"}
 end
+
+# Here I make sure that the user provides input in all fields.

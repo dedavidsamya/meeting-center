84f61970c3af0804359d2e2852fbe309f656f1fa
samya cardoso de david <dedavidsamya@gmail.com>
2021-09-16 13:14:02 +0200

    controller modified to enable user editing a booking, and modified to  redirect user to home page after deleting a booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,45 @@
 class BookingsController < ApplicationController
+  ## Here I will define the CRUD methods for my bookings
+
+  def index # all bookings
+    @bookings = Booking.all
+  end
+
+  def show
+    @booking = Booking.find(params[:id]) # Here I want to select (and show) one specific booking, thus the :id key
+  end
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.save
+    redirect_to booking_path(@booking)
+
+  end
+
+  def edit
+    @booking = Booking.find(params[:id])
+  end
+
+  def update
+    @booking = Booking.find(params[:id])
+    @booking.update(booking_params)
+    redirect_to booking_path(@booking)
+  end
+
+  def destroy
+    @booking = Booking.find(params[:id])
+    @booking.destroy
+    redirect_to booking_path
+  end
+
+  private #strong params! It passes as params only the permitted keys - this prevents external intereference (hacking)
+
+  def booking_params
+    params.require(:booking).permit(:beginning, :end, :name, :email, :catering, :seats)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,35 +1,24 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  # This is the root, home page
-  root to: 'bookings#home'
-
-  # Read all the bookings
-  get '/bookings', to: 'bookings#index'
-
-  # Read one booking
-  get '/bookings/:id', to: 'bookings#show'
-
-  # Create a booking
-  get 'bookings/new', to: 'bookings#new' # display the form to the user
-  post '/bookings', to: 'bookings#create'
-
-  # Update a booking
-  get 'bookings/:id/edit', to: 'bookings#edit'
-  patch 'bookings/:id', to: 'bookings#update'
-
-  # Delete a booking
-  delete '/bookings/:id', to: 'bookings#destroy'
-
-# ##########
-#   root to: "pages#home"
-
-#   get '/sunset', to: "pages#sunset"
-#   get '/sunrise', to: "pages#sunrise"
-#   get '/booking', to: "pages#booking"
-
+  root "bookings#new"
+  resources :bookings # rails creates the RESTful routes automatically
 end
 
- # structure of routing:
-  # verb        to: "controller#action"
-  # get '/booking', to: "pages#booking"
+  # Here is how I would create it manually:
+
+  # # Read all the bookings
+  # get '/bookings', to: 'bookings#index'
+
+  # # Create a booking
+  # get 'bookings/new', to: 'bookings#new' # display the form to the user
+  # post '/bookings', to: 'bookings#create'
+
+  # # Read one booking
+  # get '/bookings/:id', to: 'bookings#show'
+
+  # # Update a booking
+  # get 'bookings/:id/edit', to: 'bookings#edit'
+  # patch 'bookings/:id', to: 'bookings#update'
+
+  # # Delete a booking
+  # delete '/bookings/:id', to: 'bookings#destroy'
